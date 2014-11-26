41c70accdcd207c779b8064891ed5b315c471e2b
Anirban <gangopad@gmail.com>
2014-11-25 19:46:45 -0500

    committing

--- public/js/twitterStream.js
+++ public/js/twitterStream.js
@@ -1,66 +1,75 @@
 function initialize() {
-  //Setup Google Map
-  var myLatlng = new google.maps.LatLng(17.7850,-12.4183);
-  var light_grey_style = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
-  var myOptions = {
-    zoom: 2,
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    mapTypeControl: true,
-    mapTypeControlOptions: {
-      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-      position: google.maps.ControlPosition.LEFT_BOTTOM
-    },
-    styles: light_grey_style
-  };
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+    //Setup Google Map
+    var myLatlng = new google.maps.LatLng(18,-12);
+    var myOptions = {
+	zoom: 2,
+	center: myLatlng,
+	mapTypeId: google.maps.MapTypeId.ROADMAP,
+	mapTypeControl: true,
+	mapTypeControlOptions: {
+	    style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+	    position: google.maps.ControlPosition.LEFT_BOTTOM
+	},
+    };
+    var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
   
-  //Setup heat map and link to Twitter array we will append data to
-  var heatmap;
-  var liveTweets = new google.maps.MVCArray();
-  heatmap = new google.maps.visualization.HeatmapLayer({
-    data: liveTweets,
-    radius: 25
-  });
-  heatmap.setMap(map);
+    //Setup heat map and link to Twitter array we will append data to
+    var heatmap;
+    var liveTweets = new google.maps.MVCArray();
+    heatmap = new google.maps.visualization.HeatmapLayer({
+	    data: liveTweets,
+	    radius: 25
+	});
+    heatmap.setMap(map);
 
-  if(io !== undefined) {
-    // Storage for WebSocket connections
-    var socket = io.connect('http://twitterenv.elasticbeanstalk.com/');
-    //var socket = io.connect('localhost:8081');
+    console.log("before io");
+    console.log("initial filter: " + filter);
 
-    // This listens on the "twitter-steam" channel and data is 
-    // received everytime a new tweet is receieved.
-    socket.on('twitter-stream', function (data) {
+    if(io !== undefined) {
 
-      //Add tweet to the heat map array.
+	// Storage for WebSocket connections
+	//var socket = io.connect('twitternode-env-nckmmseqq5.elasticbeanstalk.com');
+	var socket = io.connect('http://localhost:8081');
 
-	    if(filter == "*" || data.text.indexOf(filter) > -1){
-      var tweetLocation = new google.maps.LatLng(data.outputPoint.lng,data.outputPoint.lat);
-      liveTweets.push(tweetLocation);
+	console.log("before twitter stream");
+	// This listens on the "twitter-steam" channel and data is 
+	// received everytime a new tweet is receieved.
+	socket.on('twitter-stream', function (data) {
+		if(filter == "*" || data.text.indexOf(filter) > -1){
+		    //Add tweet to the heat map array.
+		    var tweetLocation = new google.maps.LatLng(data.outputPoint.lng,data.outputPoint.lat);
+		    liveTweets.push(tweetLocation);
+		    //Flash a dot onto the map quickly
+		    var image;
+		    console.log(data);
+		    if(data["sentiment"] == "neutral")
+			image = "css/yellowdot.png";
+		    else if(data["sentiment"] == "positive")
+			image = "css/greendot.png";
+		    else
+			image = "css/reddot.png";
 
-      //Flash a dot onto the map quickly
-      var image = "css/small-dot-icon.png";
-      var marker = new google.maps.Marker({
-        position: tweetLocation,
-        map: map,
-        icon: image
-      });
-      setTimeout(function(){
-        marker.setMap(null);
-      },600);
+		    var title = "@" + data.author + ": " + data.text;
+		    var marker = new google.maps.Marker({
+			    position: tweetLocation,
+			    map: map,
+			    icon: image
+			});
+		    setTimeout(function(){
+			    marker.setMap(null);
+			},600);
+		}
+	    });
 
-	    }
-
-    });
-
-    // Listens for a success response from the server to 
-    // say the connection was successful.
-    socket.on("connected", function(r) {
-
-      //Now that we are connected to the server let's tell 
-      //the server we are ready to start receiving tweets.
-      socket.emit("start tweets");
-    });
-  }
+	// Listens for a success response from the server to 
+	// say the connection was successful.
+	console.log("before connected to server");
+	socket.on("connected", function(r) {
+		console.log("connected to server");
+		//Now that we are connected to the server let's tell 
+		//the server we are ready to start receiving tweets.
+		socket.emit("start tweets");
+		console.log("starting tweets"); 
+	    });
+    }
 }
---
+++ servers.js
@@ -0,0 +1,222 @@
+var gap = function(){
+    console.log("\n\n\n\n"); 
+}
+
+    var twitter = require('twitter'),
+    express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server),
+    AWS = require('aws-sdk'),
+    util = require('util'),
+    AlchemyAPI = require('alchemyapi_node'); 
+
+AWS.config.loadFromPath('./config.json'); 
+
+var sns = new AWS.SNS();
+var sqs = new AWS.SQS();
+var alchemy = new AlchemyAPI(); 
+var topicArn;
+
+// create SNS topic
+sns.createTopic({
+	'Name': 'twitternode'
+	    }, function (err, result) {
+	if (err !== null) {
+	    console.log(util.inspect(err));
+	    return;
+	}
+	topicArn = result["TopicArn"];
+	//console.log("topicArn: " + topicArn);
+
+	// create SQS queue
+	sqs.createQueue({
+		'QueueName': 'twitternode'
+		    }, function (err, result) {
+		if (err !== null) {
+		    console.log(util.inspect(err));
+		    return;
+		}
+		var queueUrl = result["QueueUrl"];
+		//console.log("queueUrl: " + queueUrl);
+
+		// get queue ARN
+		sqs.getQueueAttributes({
+			QueueUrl: queueUrl, 
+			    AttributeNames: ["QueueArn"]
+			    }, function (err, result) {
+			if (err !== null) {
+			    console.log(util.inspect(err));
+			    return;
+			}
+			var queueArn = result["Attributes"]["QueueArn"];
+			//console.log("queueArn: " + queueArn);
+
+			// subscribe 
+			sns.subscribe({
+				'TopicArn': topicArn, 
+				    'Protocol': 'sqs',
+				    'Endpoint': queueArn 
+				    }, function (err, result) {
+				if (err !== null) {
+				    console.log(util.inspect(err));
+				    return;
+				}
+				var subscriptionArn = result["SubscriptionArn"];
+				//console.log("subscriptionArn: " + subscriptionArn);
+
+				// allow topic to publish to queue
+				var attributes = {
+				    "Version": "2008-10-17",
+				    "Id": queueArn + "/SQSDefaultPolicy",
+				    "Statement": [{
+					    "Sid": "Sid" + new Date().getTime(),
+					    "Effect": "Allow",
+					    "Principal": {
+						"AWS": "*"
+					    },
+					    "Action": "SQS:SendMessage",
+					    "Resource": queueArn,
+					    "Condition": {
+						"ArnEquals": {
+						    "aws:SourceArn": topicArn
+						}
+					    }
+					}
+]
+				};
+
+				sqs.setQueueAttributes({
+					QueueUrl: queueUrl,
+					    Attributes: {
+					    'Policy': JSON.stringify(attributes)
+						}
+				    }, function (err, result) {
+					if (err !== null) {
+					    console.log(util.inspect(err));
+					    return;
+					}
+
+				    }); // end set queue attributes callback
+			    }); // end subscribe callback
+		    }); // end getQueueAttributes callback
+	    }); // end createQueue callback
+    }); // end createTopic callback
+
+
+/****************************** TESTING ****************************/
+
+// configure twitter stream api
+var twit = new twitter({
+	consumer_key: '0CNrAuSnFtRGcLvWWYzhCLuUr',
+	consumer_secret: 'izhMtz1mQzHLLlvItZgBlXsOgJk0BjKLfCSgAlx3oyeijWySc7',
+	access_token_key: '2849409910-VD66zWkCZjLS55qZhs1qnoZPMTuPGNyLi3qJRPc',
+	access_token_secret: 'ljrW3nXgDbteBP765m2rgCTu1tW9HjtTCNEoBPNTaPfAk'
+    }),
+    stream = null;
+
+// Configure app
+app.use(express.static(__dirname + '/public'));
+app.set('port', process.env.PORT || 8081);
+server.listen(app.get('port'));
+
+console.log("before receiving connection");
+//Create web sockets connection.
+io.sockets.on('connection', function (socket) {
+	console.log("after receiving connection");
+
+	socket.on("start tweets", function() {
+		console.log("starting streaming tweets");
+		// TODO: print statement here for testing SQS
+
+		if(stream === null) {
+
+		    //Connect to twitter stream passing in filter for entire world.
+		    twit.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function(stream) {
+			    stream.on('data', function(data) {
+
+				    function publish(tweet) {
+					sns.publish({
+						TargetArn: topicArn,
+						    Message: JSON.stringify(tweet)
+						    }, 
+					    function(err,data){
+						if (err){
+						    console.log("Error sending a message "+err);
+						}
+					    });
+				    }
+
+				    // Does the JSON result have coordinates
+				    if (data.coordinates){
+					if (data.coordinates !== null){
+
+					    //If so then build up some nice json and send out to web sockets
+					    var outputPoint = {"lat": data.coordinates.coordinates[0],
+							       "lng": data.coordinates.coordinates[1]};
+					    var tweet = { "outputPoint" : outputPoint,
+							  "text" : data.text,
+							  "author" : data.user.screen_name,
+					    };
+
+					    // send message to be evaluated by SNS-SQS 
+					    publish(tweet);
+
+					}
+					// Does the JSON result have a place field
+					else if(data.place){
+					    if(data.place.bounding_box === 'Polygon'){
+
+						// Calculate the center of the bounding box for the tweet
+						var coord, _i, _len;
+						var centerLat = 0;
+						var centerLng = 0;
+
+						for (_i = 0, _len = coords.length; _i < _len; _i++) {
+						    coord = coords[_i];
+						    centerLat += coord[0];
+						    centerLng += coord[1];
+						}
+						centerLat = centerLat / coords.length;
+						centerLng = centerLng / coords.length;
+
+						// Build json object
+						var outputPoint = {"lat": centerLat,"lng": centerLng};
+						var tweet = { "outputPoint" : outputPoint,
+							      "text" : data.text,
+							      "author" : data.user.screen_name,
+						};
+
+						// send message to be evaluated by SNS-SQS 
+						publish(tweet);
+
+					    }
+					}
+				    }
+				});
+			    stream.on('error', function(err) {
+				    console.log(err);
+				});
+			});
+		}
+ telling them that the
+     // they are connected and can start receiving Tweets
+     socket.emit("connected");
+ console.log("emitted connected message to client");
+
+ /************************* WORKER FOR SQS QUEUE ************************/
+ var sqs = new AWS.SQS();
+
+ sqs.listQueues(null, function(err, data){
+	 if (err) {
+	     console.log(err, erhen we've successfully processed it
+var deleteMessageParams = {
+QueueUrl: queueUrl,
+ReceiptHandle: data.Messages[0].ReceiptHandle
+};
+sqs.deleteMessage(deleteMessageParams, function(err, data){}); // end delete message callback
+readMessage();
+}); // end receive message callback
+}
+
